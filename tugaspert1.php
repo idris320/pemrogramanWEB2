9d6b79ff37fa05af1e26e0e9823742d209cdf62a
idris320 <61259555+idris320@users.noreply.github.com>
2024-03-04 23:53:13 +0700

    Menambahkan tugas pertemuan 1

---
+++ tugaspert1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TUGAS PERTEMUAN 1</title> 
+</head>
+
+<body>    
+    <div style="margin-left: 40%;">
+    <?php   
+        echo "<b style='font-size:20px; color:red; margin-left:5%;'>BIODATA</b><br>";          
+        echo "<font style='font-size: 16px; color:blue'>NAMA  :  M IDRIS EPENDI</font><br>";
+        echo "<font style='font-size: 16px; color:blue'>EMAIL  :  idrisefendi1601@gmail.com</font><br>";
+        echo "<font style='font-size: 16px; color:blue'>JURUSAN  : TEKNIK INFORMATIKA</font><br>";
+        echo "<font style='font-size: 16px; color:blue'>HOBY  :  Tenis Meja</font>";
+        ?>   
+    </div>
+</body>
+
+</html>
