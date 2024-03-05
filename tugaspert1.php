dab6d77e0d630bbdac119be65098d8d8e7936c57
idris320 <61259555+idris320@users.noreply.github.com>
2024-03-05 19:18:00 +0700

    Menambahkan Kesan pertama kali belajar php

--- tugaspert1.php
+++ tugaspert1.php
@@ -17,6 +17,13 @@
         echo "<font style='font-size: 16px; color:blue'>HOBY  :  Tenis Meja</font>";
         ?>   
     </div>
+    <br>
+    <div class="kesan" style="margin-left: 40%; margin-right:30%;">
+        <?php
+        echo "<b style='font-size:20px; color:red; margin-left:5%;'>KESAN PERTAMA BELAJAR PHP</b><br>";
+        echo "<font style='font-size: 16px; color:blue'>Belajar php untuk pertama kalinya saya sangat tertarik untuk mempelajari lagi, karena menurut artikel php ini bisa membuat web secara dinamis dan bisa terhubung dengan database untuk kedepannya.</font><br>";
+        ?>
+    </div>
 </body>
 
 </html>
