f9c5a6cdc9c614635d921def5a348e08919755df
idris320 <61259555+idris320@users.noreply.github.com>
2024-03-07 15:10:47 +0700

    Create tugaspert1.2.php

---
+++ tugaspert1.2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Pertemuan 1 no 2</title>
+</head>
+
+<body>
+    <div class="kesan" style="margin-left: 40%; margin-right:30%;">
+        <?php
+        echo "<b style='font-size:20px; color:red; margin-left:5%;'>KESAN PERTAMA BELAJAR PHP</b><br>";
+        echo "<font style='font-size: 16px; color:blue'>Belajar php untuk pertama kalinya saya sangat tertarik untuk mempelajari lagi, karena menurut artikel php ini bisa membuat web secara dinamis dan bisa terhubung dengan database untuk kedepannya.</font><br>";
+        ?>
+    </div>
+</body>
+
+</html>
