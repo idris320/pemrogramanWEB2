4a739bbc81d74094bb27a3ea6a845b7e45bca1cc
idris320 <61259555+idris320@users.noreply.github.com>
2024-03-11 00:03:08 +0700

    Memindahkan Tugas Pertemuan 1 kedalam folder

---
+++ PERTEMUAN1/tugaspert1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TUGAS PERTEMUAN 1</title>
+</head>
+
+<body>
+    <div style="margin-left: 40%;">
+        <?php
+        echo "<b style='font-size:20px; color:red; margin-left:5%;'>BIODATA</b><br>";
+        echo "<font style='font-size: 16px; color:blue'>NAMA  :  M IDRIS EPENDI</font><br>";
+        echo "<font style='font-size: 16px; color:blue'>EMAIL  :  idrisefendi1601@gmail.com</font><br>";
+        echo "<font style='font-size: 16px; color:blue'>JURUSAN  : TEKNIK INFORMATIKA</font><br>";
+        echo "<font style='font-size: 16px; color:blue'>HOBY  :  Tenis Meja</font>";
+        ?>
+    </div><br>
+    <div class="kesan" style="margin-left: 40%; margin-right:30%;">
+        <?php
+        echo "<b style='font-size:20px; color:red; margin-left:5%;'>KESAN PERTAMA BELAJAR PHP</b><br>";
+        echo "<font style='font-size: 16px; color:blue'>Belajar php untuk pertama kalinya saya sangat tertarik untuk mempelajari lagi, karena menurut artikel php ini bisa membuat web secara dinamis dan bisa terhubung dengan database untuk kedepannya.</font><br>";
+        ?>
+    </div>
+</body>
+
+</html>
